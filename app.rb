3551a5f65b512a3caba13479b06eadf6a40bd207
Lawson Kurtz <lawson.kurtz@viget.com>
2013-03-14 14:37:51 -0400

    Cleaned up

--- app.rb
+++ app.rb
@@ -5,45 +5,25 @@ require 'active_model_monkeypatch'
 require 'deal'
 require 'utility'
 
-require 'pry'
-require 'logger'
-
-ActiveResource::Base.logger = Logger.new(STDOUT)
-
 class HighriseApp < Sinatra::Base
   enable :sessions
   use Rack::Flash
 
-  def boot_highrise
+  before do
     secrets = YAML.load_file('config/secrets.yml')
-
     @highrise_url = secrets.fetch('highrise')['site']
     Highrise::Base.site = @highrise_url
     Highrise::Base.user = secrets.fetch('highrise')['api-token']
   end
 
   get "/deal/:deal_id/edit" do
-    boot_highrise
     @deal = Highrise::Deal.find(params[:deal_id])
     erb :edit
   end
 
   post "/deal/:deal_id" do
-    boot_highrise
-
     @deal = Highrise::Deal.find(params[:deal_id])
-
-    request.POST.each do |type, array|
-      array.each do |key, value|
-        value = case type
-          when "int" then value.to_i
-          when "date" then Date.parse(value)
-        end
-        @deal.forecast_data.send("#{key}=", value)
-      end
-    end
-
-    @deal.write_forecast_data
+    @deal.update_forecast_data(request.POST)
     @deal.attributes.reject!{|k,v| k == "party"}
     saved = @deal.save
 
@@ -57,9 +37,7 @@ class HighriseApp < Sinatra::Base
   end
 
   get "/" do
-    boot_highrise
     @deals = Highrise::Deal.find(:all)
-
     erb :index
   end
 end
--- lib/deal.rb
+++ lib/deal.rb
@@ -68,6 +68,19 @@ module HighriseExtension
       @forecast_data ||= read_forecast_data
     end
 
+    def update_forecast_data(data)
+      data.each do |type, array|
+        array.each do |key, value|
+          value = case type
+            when "int" then value.to_i
+            when "date" then Date.parse(value)
+          end
+          forecast_data.send("#{key}=", value)
+        end
+      end
+      write_forecast_data
+    end
+
     def write_forecast_data
       # How can I get this into a callback on save? before_save isn't working even after including ActiveResource::Callbacks.
       self.background = ForecastDataIO.write(self, :forecast_data, :background)
